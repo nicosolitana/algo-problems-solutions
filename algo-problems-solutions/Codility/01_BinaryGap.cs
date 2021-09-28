b9bc9c47003c112012c56bd3b8df28faacfca0a4
Your Namenicosolitna <you@example.com>
2021-09-29 02:32:41 +0800

    Adding source code solutions for codility  problems.

---
+++ algo-problems-solutions/Codility/01_BinaryGap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace algo_problems_solutions.Codility
+{
+    class _01_BinaryGap
+    {
+        // Time Complexity is O(N)
+        // where is the number of binary digits of an integer number
+        public int GetMexBinaryGap(int N)
+        {
+            string binary = Convert.ToString(N, 2);
+            bool start = false;
+            int gapCount = 0, maxGap = 0;
+            foreach (char b in binary)
+            {
+                if (b == '1')
+                {
+                    if (start == false)
+                    {
+                        start = true;
+                    }
+                    else
+                    {
+                        if (maxGap < gapCount)
+                            maxGap = gapCount;
+                        gapCount = 0;
+                    }
+                }
+
+                if ((start == true) && (b != '1'))
+                {
+                    gapCount++;
+                }
+            }
+            return maxGap;
+        }
+    }
+}
---
+++ algo-problems-solutions/Codility/02_CyclicRotation.cs
@@ -0,0 +1,33 @@
+﻿namespace algo_problems_solutions.Codility
+{
+    class _02_CyclicRotation
+    {
+        public int[] solution(int[] A, int K)
+        {
+            if ((K == 0) ||
+                (A.Length == 1))
+                return A;
+
+            int SIZE = A.Length;
+            int[] nA = new int[SIZE];
+
+            for (int i = 0; i < SIZE; i++)
+            {
+                int pos = i + K;
+                if (pos >= SIZE)
+                {
+                    int k_num = K;
+                    while (pos >= SIZE)
+                    {
+                        pos = k_num - (SIZE - i);
+                        k_num = pos;
+                    }
+                }
+
+                nA[pos] = A[i];
+            }
+
+            return nA;
+        }
+    }
+}
