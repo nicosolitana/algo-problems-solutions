248cee8400962e6e5f89667febc04d5e4781e97c
Your Namenicosolitna <you@example.com>
2021-10-02 16:41:13 +0800

    Added some more solutions on codility problems.

---
+++ algo-problems-solutions/Codility/17_Denominator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace algo_problems_solutions.Codility
+{
+    class _17_Denominator
+    {
+        // Time Complexity is O(N^2)
+        // Correctness OK, Performance NG. Can reach hard limit
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (A.Length == 0)
+                return -1;
+
+            if (A.Length == 1)
+                return 0;
+
+            int[] B = new int[A.Length];
+            for (int i = 0; i < A.Length; i++)
+            {
+                B[i] = A.Where(x => x.Equals(A[i])).Count();
+            }
+            int index = Array.IndexOf(B, B.Max());
+
+            if (B[index] > (A.Length / 2))
+            {
+                return index;
+            }
+            return -1;
+        }
+
+        // Time Complexity is O(N) but can also reach 
+        // O(N log N)
+        public int solution2(int[] A)
+        {
+            if (A.Length == 0)
+                return -1;
+
+            if (A.Length == 1)
+                return 0;
+
+            var value = (from item in A
+                         group item by item into g
+                         orderby g.Count() descending
+                         select g.Key).First();
+
+            int count = A.Where(x => x.Equals(value)).Count();
+            int index = Array.IndexOf(A, value);
+
+            if (count > (A.Length / 2))
+            {
+                return index;
+            }
+            return -1;
+        }
+    }
+}
---
+++ algo-problems-solutions/Codility/18_MaxProfit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace algo_problems_solutions.Codility
+{
+    class _18_MaxProfit
+    {
+        // Time Complexity is O(N^2)
+        // Might reach hard limit
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (A.Length == 0)
+                return 0;
+
+            int max = Int32.MinValue;
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    int diff = A[j] - A[i];
+                    max = Math.Max(max, diff);
+                }
+            }
+            if (max < 0)
+                return 0;
+
+            return max;
+        }
+
+        //Time Complexity is O(N)
+        public int solution2(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (A.Length == 0)
+                return 0;
+
+            int minValue = A[0];
+            int maxValue = 0;
+            for (int i = 1; i < A.Length; i++)
+            {
+                int max = Math.Max(0, A[i] - minValue);
+                minValue = Math.Min(A[i], minValue);
+                maxValue = Math.Max(max, maxValue);
+            }
+            return maxValue;
+        }
+    }
+}
